fcc3f96ecd23c5a25de5fe1af6597ee079140ab5
OfirBenYosef <73785427+OfirBenYosef@users.noreply.github.com>
2020-12-08 19:44:14 +0200

    Update scrape_news.sh

--- scrape_news.sh
+++ scrape_news.sh
@@ -4,7 +4,7 @@
 # Find all the articles which fits to the instructions, 
 # and make sure that there is only a single copy of each.
 wget https://www.ynetnews.com/category/3082
-grep -i -o -E 'https://www.ynetnews.com/article/[a-zA-Z0-9]{9}["|#]' 3082      \
+grep -i -o -E 'https://www.ynetnews.com/article/[a-zA-Z0-9]{9}["|#]' 3082                         \
 | cat > art.txt
 awk -F '"|#' '{print $1}' art.txt | cat > cart1.txt
 sort cart1.txt | uniq | cat > cart.txt
@@ -19,23 +19,23 @@ sort temp.txt | uniq -c | cat > temp1.txt
 
 # Rearrange the information we found to match the exercise format.
 sed -i 's/:/ /g' temp1.txt
-awk '{art[NR]=$2; man[NR]=$3; num[NR]=$1}                                      \
-		END{                                                                   \
-			for (i=1; i<NR ; i++) {									           \	
-				if (art[i]==art[i+1]) {                                        \
+awk '{art[NR]=$2; man[NR]=$3; num[NR]=$1}                                                          \
+		END{                                                                               \
+			for (i=1; i<NR ; i++) {						           \	
+				if (art[i]==art[i+1]) {                                            \
 					print art[i]",", "Netanyahu,",                             \
 					 num[i+1]",", "Gantz,", num[i];                            \
-				}                                                              \
-				else if(art[i+1]!=art[i+2]){                                   \
+				}                                                                  \
+				else if(art[i+1]!=art[i+2]){                                       \ 
 					if(man[i+1]=="Netanyahu"){                                 \
-						print art[i+1]",", "Netanyahu,",                       \
-						 num[i+1]",", "Gantz,", "0";                           \
+						print art[i+1]",", "Netanyahu,",                   \
+						 num[i+1]",", "Gantz,", "0";                       \
 					}                                                          \
 					else{                                                      \
 					print art[i+1]",", "Netanyahu,", "0,", "Gantz,", num[i+1]; \
 					}                                                          \
-				}                                                              \
-			}                                                                  \
+				}                                                                  \
+			}                                                                          \
 		}' temp1.txt | cat > cart2.txt
 		
 # Copy the organized information into the upper directory.
